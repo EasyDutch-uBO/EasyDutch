6ddf4a532d68d3bfb684eeaec0a2aa3117e388a5
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2023-05-03 12:51:11 +0000

    Update make-easydutch.sh

--- tools/make-easydutch.sh
+++ tools/make-easydutch.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+#
+# This script assumes a linux environment
+
 echo "*** EasyDutch: Assembling EasyDutch.txt"
 node ./tools/make-list.js in=tool/easydutch-filters.template out=EasyDutch.min.txt minify=1
