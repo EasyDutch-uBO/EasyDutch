fb0d90954d2735441f622af4ff4140ee2a87cddc
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2023-05-03 12:24:14 +0000

    Create make-list.js

---
+++ tools/make-list.js
@@ -0,0 +1,125 @@
+'use strict';
+
+/******************************************************************************/
+
+import fs from 'fs/promises';
+import path from 'path';
+import process from 'process';
+
+/******************************************************************************/
+
+const expandedParts = new Set();
+
+/******************************************************************************/
+
+const commandLineArgs = (( ) => {
+    const args = new Map();
+    let name, value;
+    for ( const arg of process.argv.slice(2) ) {
+        const pos = arg.indexOf('=');
+        if ( pos === -1 ) {
+            name = arg;
+            value = '';
+        } else {
+            name = arg.slice(0, pos);
+            value = arg.slice(pos+1);
+        }
+        args.set(name, value);
+    }
+    return args;
+})();
+
+/******************************************************************************/
+
+function expandIncludes(dirname, parts) {
+    const out = [];
+    const reInclude = /^%include +(.+):(.+)%\s+/gm;
+    for ( const part of parts ) {
+        let lastIndex = 0;
+        for (;;) {
+            const match = reInclude.exec(part);
+            if ( match === null ) { break; }
+            const repo = match[1].trim();
+            const fpath = match[2].trim();
+            const basename = path.basename(fpath);
+            expandedParts.add(basename);
+            console.info(`  Inserting ${basename}`);
+            out.push(
+                part.slice(lastIndex, match.index).trim(),
+                '',
+                `! *** ${repo}:${fpath} ***`,
+                fs.readFile(`${dirname}/${fpath}`, { encoding: 'utf8' })
+                    .then(text => text.trim()),
+            );
+            lastIndex = reInclude.lastIndex;
+        }
+        out.push(part.slice(lastIndex).trim());
+    }
+    return out;
+}
+
+/******************************************************************************/
+
+function removeIncludeDirectives(text) {
+    const excludedParts = Array.from(expandedParts)
+        .map(s => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'))
+        .join('|');
+    const re = new RegExp(`^!#include +(${excludedParts}).*$`, 'm');
+    const out = [];
+    for (;;) {
+        const match = re.exec(text);
+        if ( match === null ) { break; }
+        console.info(`  Removing "!#include ${match[1]}"`);
+        out.push(text.slice(0, match.index).trim());
+        text = text.slice(match.index + match[0].length).trim();
+    }
+    out.push(text.trim(), '\n');
+    return out.join('\n');
+}
+
+/******************************************************************************/
+
+function minify(text) {
+    // remove issue-related comments
+    text = text.replace(/^! https:\/\/[^\n\r]+[\n\r]+/gm, '');
+    // remove empty lines
+    text = text.replace(/^[\n\r]+/gm, '');
+    return text;
+}
+
+/******************************************************************************/
+
+async function main() {
+    const workingDir = commandLineArgs.get('dir') || '.';
+    const inFile = commandLineArgs.get('in');
+    if ( typeof inFile !== 'string' || inFile === '' ) {
+        process.exit(1);
+    }
+    const outFile = commandLineArgs.get('out');
+    if ( typeof outFile !== 'string' || outFile === '' ) {
+        process.exit(1);
+    }
+
+    console.info(`  Using template at ${inFile}`);
+
+    const inText = fs.readFile(`${workingDir}/${inFile}`, { encoding: 'utf8' });
+
+    let parts = [ inText ];
+    do {
+        parts = await Promise.all(parts);
+        parts = expandIncludes(workingDir, parts);
+    } while ( parts.some(v => typeof v !== 'string'));
+
+    let afterText = parts.join('\n') + '\n';
+    afterText = removeIncludeDirectives(afterText);
+
+    if ( commandLineArgs.get('minify') !== undefined ) {
+        afterText = minify(afterText);
+    }
+
+    console.info(`  Creating ${outFile}`);
+
+    fs.writeFile(outFile, afterText);
+}
+
+main();
