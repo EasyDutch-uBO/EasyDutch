6a4830944458323e44892561bfad8bc1b30b1d5c
BPower0036 <80090789+BPower0036@users.noreply.github.com>
2023-05-03 12:31:30 +0000

    Update make-easydutch.sh

--- tools/make-easydutch.sh
+++ tools/make-easydutch.sh
@@ -1,2 +1,2 @@
 echo "*** EasyDutch: Assembling EasyDutch.txt"
-node ./tools/make-list.js in=easydutch-filters.template out=EasyDutch.min.txt minify=1
+node ./tools/make-list.js in=tool/easydutch-filters.template out=EasyDutch.min.txt minify=1
