88c78a3c26db457ef4dd81718a521b7ae457a5a6
Nomes77 <80090789+Nomes77@users.noreply.github.com>
2023-10-31 05:42:25 +0000

    Update make-easydutch.sh

--- tools/make-easydutch.sh
+++ tools/make-easydutch.sh
@@ -3,4 +3,4 @@
 # This script assumes a linux environment
 
 echo "*** EasyDutch: Assembling EasyDutch.txt"
-node ./tools/make-list.js in=tools/easydutch-filters.template out=EasyDutch.out.txt
+node ./tools/make-list.js in=tools/easydutch-filters.template out=EasyDutch.out.txt minify=1
